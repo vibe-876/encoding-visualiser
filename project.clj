02bf5ecc891c6f2a19478fc3e0ee81cdc2e72d3c
cam <cameronayres123@gmail.com>
2025-09-04 15:34:55 +0100

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject encoding-visualiser "0.1.0-SNAPSHOT"
+  :description "Learning tool for R.L.E.."
+  :url "https://github.com/vibe-876/encoding-visualiser"
+  :license {:name "GPLv3"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.htmlx"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main ^:skip-aot encoding-visualiser.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
---
+++ test/encoding_visualiser/core_test.clj
@@ -0,0 +1,7 @@
+(ns encoding-visualiser.core-test
+  (:require [clojure.test :refer :all]
+            [encoding-visualiser.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
